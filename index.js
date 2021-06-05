a40fd366ecfb074df437150315d59361a4c031fa
Craig Norford <craig.a.norford@gmail.com>
2021-06-06 00:14:17 +0100

    post input error handling

--- index.js
+++ index.js
@@ -68,13 +68,27 @@ const generateId = () => {
 // route to post items
 app.post('/api/persons', (req,res) => {
     const body = req.body
-    console.log(body)
+    // input error handling
+    if (!body.name || !body.number){
+        return res.status(400).json({
+            error: `${!body.name ? !body.number ? 'Person must have a name and number': 'person must have a name' : 'person must have a number'}`
+        })
+    }
+    //check if name already exist
+    for (let person in persons){
+        if (person.name === body.name){
+            return res.status(400).json({
+                error: 'Name already exists'
+            })        
+        }
+    }
+    //console.log(body)
     const newPerson = {
         id: generateId(),
         name: body.name,
         number: body.number
     }
-    console.log(newPerson);
+    //console.log(newPerson);
     persons = persons.concat(newPerson)
 
     res.json(newPerson)
